c4174bd2f799599b46d3415460a6e2aae4139d32
Rain Yang <76612093+RainYangty@users.noreply.github.com>
2023-10-04 15:40:04 +0800

    Update main.py
    1，增加四川地震局提供的预警能力系统
    2，异步检测CENC与四川地震局提供的速报与预警

--- main.py
+++ main.py
@@ -37,10 +37,10 @@ while True:
             time.sleep(1)
             arrivetime -= 1
             if localmagnitude < 5.0 and startarrti - arrivetime < 4:
-                countdown(arrivetime = arrivetime, pos = response.json()['No0']['location'], localmagnitude = localmagnitude, startarrti=startarrti)
+                countdown(arrivetime = arrivetime, pos = pos, localmagnitude = localmagnitude, startarrti=startarrti)
                 return
             if localmagnitude >= 5.0 and startarrti - arrivetime < 13:
-                countdown(arrivetime = arrivetime, pos = response.json()['No0']['location'], localmagnitude = localmagnitude, startarrti=startarrti)
+                countdown(arrivetime = arrivetime, pos = pos, localmagnitude = localmagnitude, startarrti=startarrti)
                 return
             if localmagnitude < 5.0 and startarrti - arrivetime >= 4 and arrivetime % 10 != 0 and arrivetime % 10 != 9:
                 playsound(r"/home/RainYangty/audio/countdown/ding.mp3")   #播放地震倒计时
@@ -61,7 +61,7 @@ while True:
             if arrivetime <= 0:
                 return
             else:
-                countdown(arrivetime = arrivetime, pos = response.json()['No0']['location'], localmagnitude = localmagnitude, startarrti=startarrti)
+                countdown(arrivetime = arrivetime, pos = pos, localmagnitude = localmagnitude, startarrti=startarrti)
                 return
 
         def countdownau(arrivetime, pos, localmagnitude):     #倒计时
@@ -80,83 +80,121 @@ while True:
     except:
         continue
 
-while True:
-    ctime = int(time.time() * 1000)
-    errtime = datetime.datetime.now()   #避免因网络错误产生高延迟 导致反馈错误的时间不准
-    # print("get json")
-    try:
-        response = requests.get("https://api.wolfx.jp/cenc_eqlist.json", timeout = 200)  #设置等待时间，若无响应则网络出现问题： https://api.wolfx.jp/cenc_eqlist.json
-    except:
-        if err == False:
-            print(str(errtime) + "网络错误")
-            err = True
-        time.sleep(1)
-        continue
-
-    if err == True: 
-        print(str(datetime.datetime.now()) + "网络恢复啦o((>ω< ))o")
-        err = False
-
-    # print(response.json()['md5'])
-
-    if response.json()['md5'] != lastmd5:
-        #play = Thread(target=playsound, args = (r"/home/RainYangty/audio/cenc.mp3"))    #启动新线程播放地震发现声音
-        #play.start()
-        playsound(r"/home/RainYangty/audio/cenc.mp3")
-
-        lastmd5 = response.json()['md5']
-        #计算与震源距离（单位km）
-        print(str(datetime.datetime.now()) + "检测到地震API变化( " + response.json()['No0']['location'] + " 发生地震)计算距离")
-        tlength = length(float(response.json()['No0']['latitude']), float(response.json()['No0']['longitude']))
-        print(str(datetime.datetime.now()) + "距离: " + str(int(tlength)) + " km")
-
-        #修正时间，按横波（取4km/s）抵达时间计算
-        timeArray = time.strptime(response.json()['No0']['time'], "%Y-%m-%d %H:%M:%S")
-        timeStamp = time.mktime(timeArray)
-        arrivetime = tlength / 4 - int(time.time() - timeStamp)
-        if arrivetime > 0:
-            print(str(datetime.datetime.now()) + "时间修正完毕, " + str(int(arrivetime)) + "s 后抵达(S波)")
-        else:
-            print(str(datetime.datetime.now()) + "时间修正完毕, " + str(int(0 - arrivetime)) + "s 前已抵达(S波)")
-
-        if arrivetime >= -120: #若S波已抵达超过120s则不再反馈
-            if response.json()['No0']['type'] == "reviewed":
+def cenc():
+    print("cenc")
+    #cenc测定播报
+    lastmd5 = 0
+    while True:
+        ctime = int(time.time() * 1000)
+        errtime = datetime.datetime.now()   #避免因网络错误产生高延迟 导致反馈错误的时间不准
+        err = True
+        try:
+            eqlist = requests.get("https://api.wolfx.jp/cenc_eqlist.json", timeout = 200) #设置等待时间，若无响应则网络出现问题： https://api.wolfx.jp/cenc_eqlist.json
+        except:
+            if err == False:
+                print(str(errtime) + "网络错误")
+                err = True
+            time.sleep(1)
+            continue
+        if eqlist.json()['md5'] != lastmd5:
+            playsound(r"/home/RainYangty/audio/cenc.mp3")
+            lastmd5 = eqlist.json()['md5']
+            etype = ""
+            if eqlist.json()['No0']['type'] == "reviewed":
                 etype = "正式测定"
             else:
                 etype = "自动测定"
-            print(str(datetime.datetime.now()) + "播报类型为" + etype)
 
-            #计算本地震度
-            print(str(datetime.datetime.now()) + "计算本地震度")
-            localmagnitude = 0.92 + 1.63 * float(response.json()['No0']['magnitude']) - 3.49 * math.log10(tlength)
+            tlength = length(float(eqlist.json()['No0']['latitude']), float(eqlist.json()['No0']['longitude'])) #距离
+
+            localmagnitude = 0.92 + 1.63 * float(eqlist.json()['No0']['magnitude']) - 3.49 * math.log10(tlength) #震级
             if localmagnitude <= 0:
-                localmagnitude = 0.0
+                    localmagnitude = 0.0
             elif localmagnitude < 12:
                 localmagnitude = int(localmagnitude * 10) / 10.0    #保留1位小数
             elif localmagnitude >= 12:
                 localmagnitude = 12.0
-            print(str(datetime.datetime.now()) + "震度为" + str(localmagnitude) + "级")
-            
-            print(str(datetime.datetime.now()) + "将获取数据发送")
-            if arrivetime > 0:
-                arrivetime = tlength / 4 - int(time.time() - timeStamp)     #修正因发送前文导致的时间延时
-                if localmagnitude >= 3.0:
-                    #play = Thread(target=playsound, args = (r"/home/RainYangty/audio/cenc.mp3"))    #启动新线程播放有感地震警报
-                    #play.start()
-                    if localmagnitude >= 5.0:
-                        playsound(r"/home/RainYangty/audio/eew2.mp3")
-                    else:
-                        playsound(r"/home/RainYangty/audio/eew1.mp3")
-                    print(print(str(datetime.datetime.now()) + "本地超过 3.0 级，启动倒计时"))
-                    count = Thread(target=countdown, args = (int(arrivetime), response.json()['No0']['location'], localmagnitude, int(arrivetime)))    #启动新线程倒计时
-                    count.start()
-                msg = response.json()['No0']['location'] + "(" + response.json()['No0']['latitude'] + ", " + response.json()['No0']['longitude'] + ")于" + response.json()['No0']['time'] + "发生" + response.json()['No0']['magnitude'] + "级地震, " + "距离震中" + str(int(tlength)) + "km" + "   估计本地" + str(localmagnitude) + "级 " + "    预计抵达时间(S波)" + str(int(arrivetime)) + "s (" + etype + ")"
-            else:
-                msg = response.json()['No0']['location'] + "(" + response.json()['No0']['latitude'] + ", " + response.json()['No0']['longitude'] + ")于" + response.json()['No0']['time'] + "发生" + response.json()['No0']['magnitude'] + "级地震, " + "距离震中" + str(int(tlength)) + "km" + "   估计本地" + str(localmagnitude) + "级 " + "    已抵达(S波) (" + etype + ")"
-            robot.send_text(msg = msg, at_mobiles = at_mobiles)
-            print(str(datetime.datetime.now()) + "发送成功")
-        else:
-            print(str(datetime.datetime.now()) + "抵达时间超过120s, 不再发送")
 
-    
-    time.sleep(1)
+            print(str(datetime.datetime.now()) + " cenc" + etype + "( " + eqlist.json()['No0']['location'] + " 发生地震)距离: " + str(int(tlength)) + " km 本地震级：" + str(localmagnitude))
+            robot.send_text(msg = str(datetime.datetime.now()) + " cenc" + etype + "( " + eqlist.json()['No0']['location'] + " 发生地震)距离: " + str(int(tlength)) + " km 本地震级：" + str(localmagnitude), at_mobiles = at_mobiles)
+        time.sleep(1)
+
+def sc_eew():
+    print("sc_eew")
+    eewlastid = 0
+    while True:
+        ctime = int(time.time() * 1000)
+        errtime = datetime.datetime.now()   #避免因网络错误产生高延迟 导致反馈错误的时间不准
+        # print("get json")
+        try:
+            eewwarn = requests.get("https://api.wolfx.jp/sc_eew.json", timeout = 200)   #设置等待时间，若无响应则网络出现问题：https://api.wolfx.jp/sc_eew.json
+        except:
+            if err == False:
+                print(str(errtime) + "网络错误")
+                err = True
+            time.sleep(1)
+            continue
+        #eew预警
+        if eewwarn.json()['EventID'] != eewlastid:
+            #play = Thread(target=playsound, args = (r"/home/RainYangty/audio/cenc.mp3"))    #启动新线程播放地震发现声音
+            #play.start()
+            playsound(r"/home/RainYangty/audio/update.mp3")
+
+            eewlastid = eewwarn.json()['EventID']
+            #计算与震源距离（单位km）
+            print(str(datetime.datetime.now()) + eewwarn.json()['HypoCenter'] + " 发生地震(sc_eew预警) 计算距离")
+            tlength = length(float(eewwarn.json()['Latitude']), float(eewwarn.json()['Longitude']))
+            print(str(datetime.datetime.now()) + "距离: " + str(int(tlength)) + " km")
+
+            #修正时间，按横波（取4km/s）抵达时间计算
+            timeArray = time.strptime(eewwarn.json()['OriginTime'], "%Y-%m-%d %H:%M:%S")
+            timeStamp = time.mktime(timeArray)
+            arrivetime = tlength / 4 - int(time.time() - timeStamp)
+
+            if arrivetime > 0:
+                print(str(datetime.datetime.now()) + "时间修正完毕, " + str(int(arrivetime)) + "s 后抵达(S波)")
+            else:
+                print(str(datetime.datetime.now()) + "时间修正完毕, " + str(int(0 - arrivetime)) + "s 前已抵达(S波)")
+
+            if arrivetime >= -120: #若S波已抵达超过120s则不再反馈
+                
+                #计算本地震度
+                print(str(datetime.datetime.now()) + "计算本地震度")
+                localmagnitude = 0.92 + 1.63 * float(eewwarn.json()['Magnitude']) - 3.49 * math.log10(tlength)
+                if localmagnitude <= 0:
+                    localmagnitude = 0.0
+                elif localmagnitude < 12:
+                    localmagnitude = int(localmagnitude * 10) / 10.0    #保留1位小数
+                elif localmagnitude >= 12:
+                    localmagnitude = 12.0
+                print(str(datetime.datetime.now()) + "震度为" + str(localmagnitude) + "级")
+                
+                print(str(datetime.datetime.now()) + "将获取数据发送")
+                if arrivetime > 0:
+                    arrivetime = tlength / 4 - int(time.time() - timeStamp)     #修正因发送前文导致的时间延时
+                    if localmagnitude >= 0.0:
+                        #play = Thread(target=playsound, args = (r"/home/RainYangty/audio/cenc.mp3"))    #启动新线程播放有感地震警报
+                        #play.start()
+                        if localmagnitude >= 5.0:
+                            playsound(r"/home/RainYangty/audio/eew2.mp3")
+                        else:
+                            playsound(r"/home/RainYangty/audio/eew1.mp3")
+                        print(print(str(datetime.datetime.now()) + "本地超过 3.0 级，启动倒计时"))
+                        count = Thread(target=countdown, args = (int(arrivetime), eewwarn.json()['HypoCenter'], localmagnitude, int(arrivetime)))    #启动新线程倒计时
+                        count.start()
+                    msg = eewwarn.json()['HypoCenter'] + "(" + eewwarn.json()['Latitude'] + ", " + eewwarn.json()['Longitude'] + ")于" + eewwarn.json()['OriginTime'] + "发生" + eewwarn.json()['Magnitude'] + "级地震, " + "距离震中" + str(int(tlength)) + "km" + "   估计本地" + str(localmagnitude) + "级 " + "    预计抵达时间(S波)" + str(int(arrivetime)) + "s"
+                else:
+                    msg = eewwarn.json()['HypoCenter'] + "(" + eewwarn.json()['Latitude'] + ", " + eewwarn.json()['Longitude'] + ")于" + eewwarn.json()['OriginTime'] + "发生" + eewwarn.json()['Magnitude'] + "级地震, " + "距离震中" + str(int(tlength)) + "km" + "   估计本地" + str(localmagnitude) + "级 " + "    已抵达(S波) "
+                robot.send_text(msg = msg, at_mobiles = at_mobiles)
+                print(str(datetime.datetime.now()) + "发送成功")
+            else:
+                print(str(datetime.datetime.now()) + "抵达时间超过120s, 不再发送")
+
+        
+        time.sleep(1)
+
+eqli = Thread(target=cenc)
+eewwa = Thread(target=sc_eew)
+
+eqli.start()
+eewwa.start()
