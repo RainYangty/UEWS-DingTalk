8d617698b8b4425df5c1331b5e3df61f5d07d915
Rain Yang <76612093+RainYangty@users.noreply.github.com>
2024-01-20 10:31:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import os
 webhook = 'https://oapi.dingtalk.com/robot/send?access_token=你的token'
 secret = 'SEC...你的密钥'  # 可选：创建机器人勾选“加签”选项时使用
 robot = DingtalkChatbot(webhook, secret=secret)  # 方式二：勾选“加签”选项时使用（v1.5以上新功能）
-location = [31.75803, 117.253804]    #你的坐标 [纬度, 经度] 默认为 合肥一六八中学
+location = [31.75803, 117.253804]    #你的坐标 [纬度, 经度] 默认为 合肥一六八中学 嘿嘿
 at_mobiles = ['']    #填写你注册钉钉的手机号码
 path = os.path.abspath('.')
 ewarn = False
